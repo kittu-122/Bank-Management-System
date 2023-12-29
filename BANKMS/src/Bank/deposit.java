647bc26fed0de722ad8ce69e2ff688959992d081
Kirti Rane <139479268+kittu-122@users.noreply.github.com>
2023-12-29 18:52:59 +0530

    Add files via upload

---
+++ BANKMS/src/Bank/deposit.java
@@ -0,0 +1,93 @@
+package Bank;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class deposit extends JFrame implements ActionListener {
+    JTextField amount;
+    JLabel text;
+    JButton deposit,back;
+    String pinnumber;
+    deposit(String pinnumber)
+    {
+        this.pinnumber=pinnumber;
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,900,900);
+        add(image);
+
+        text = new JLabel("Enter the amount you want to deposit");
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("ARABIC",Font.BOLD,16));
+        text.setBounds(180,300,400,20);
+        image.add(text);
+
+        amount = new JTextField();
+        amount.setFont(new Font("RALEWAY",Font.BOLD,22));
+        amount.setBounds(170,350,320,25);
+        image.add(amount);
+
+        deposit = new JButton("Deposit");
+        deposit.setBounds(355,485,150,30);
+        deposit.addActionListener(this);
+        image.add(deposit);
+
+        back = new JButton("Back");
+        back.setBounds(355,520,150,30);
+        back.addActionListener(this);
+        image.add(back);
+
+
+        setSize(900,900);
+        setLocation(300,0);
+        setUndecorated(true);
+        setVisible(true);
+    }
+
+
+    public void actionPerformed (ActionEvent ae)
+    {
+        if(ae.getSource()==deposit)
+        {
+            String number=amount.getText();
+            Date date = new Date();
+            if(number.equals(" "))
+            {
+                JOptionPane.showMessageDialog(null,"PLEASE ENTER THE AMOUNT YOU WANT TO DEPOSIT");
+            }
+            else {
+                try {
+                    Conn conn = new Conn();
+                    String query = "insert into bank value ('" + pinnumber + "','" + date + "','" + deposit + "','" + number + "')";
+                    conn.s.executeUpdate(query);
+                    JOptionPane.showMessageDialog(null, "Rs " + number + "DEPOSITED SUCCESSFULLY");
+                    new Transaction(pinnumber).setVisible(true);
+                }
+                catch (Exception e)
+                {
+                    System.out.println(e);
+                }
+            }
+        }
+        else if(ae.getSource()==back)
+        {
+            setVisible(false);
+            new Transaction(pinnumber).setVisible(true);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+      new deposit(" ");
+    }
+
+
+
+}
